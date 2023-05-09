10216802e57179e8337bfd0442c69e8d4376be7b
BitProxy <129908747+BitProxy@users.noreply.github.com>
2023-05-08 20:36:45 -0700

    Add files via upload

---
+++ detections.js
@@ -0,0 +1,8 @@
+function checkurl(URL) {
+    if (URL == "https://bitproxy.github.io/404.html") {
+        return "This url exists, used for non-existance files."
+    }
+    return "File cannot be found."
+}
+
+// More functions adding sooner..
