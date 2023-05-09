799aaf5b5d74303e0abbd1a50d5032f731dc755b
BitProxy <129908747+BitProxy@users.noreply.github.com>
2023-05-08 21:42:52 -0700

    Add files via upload

---
+++ webproxies.js
@@ -0,0 +1,21 @@
+var Proxies = {}
+
+function openproxy(name) {
+    if (Proxies[name]) {
+        if (Proxies[name].Status == "Slow") {
+            alert("This website may be running slow. Keep in mind this website was last checked on " + Proxies[name].Updated)
+        }
+        window.open(Proxies[name].Url)
+    }
+}
+
+fetch('https://bitproxy.github.io/webproxies.json')
+  .then(response => response.json())
+  .then(data => {
+    // Setting response data
+    Proxies = data
+  })
+  .catch(error => {
+    // Handling error requests
+    console.error('HTTP Error: ', error);
+  });
