954dff723874b1e753fb7d8cb57935dd8c1023f0
BitProxy <129908747+BitProxy@users.noreply.github.com>
2023-05-08 19:21:06 -0700

    Update rblxpro.js

--- rblxpro.js
+++ rblxpro.js
@@ -1,4 +1,3 @@
-var web = "https://bitproxy.github.io/data.json"
 var JSON_DATA = {"MainProfile":94111, PublicAnnouncement:"N/A", LastUpdated:"Mar 2nd 2021 AT 12:32:46 PDT"}
 
 function getmainprofile() {
@@ -12,12 +11,15 @@ function openrblxprofile(profile) {
     window.open("https://roblox.com/users/" + toString(profile) + "/profile")
 }
 
-function setjsonData(any) {
-    JSON_DATA = any
-}
-
 // HTTP Request 'fetch'
 
-fetch(web)
-    .then(response => response.json())
-    .then(data => setjsonData(data))
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Setting response data
+    JSON_DATA = data
+  })
+  .catch(error => {
+    // Handling error requests
+    console.error('HTTP Error: ', error);
+  });
